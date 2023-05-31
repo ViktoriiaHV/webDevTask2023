fdf25b05c0bff31b72b3a64c11c64a1d56ed4a5f
VIKTORIIA HOLOVKO <golovkovictoria@gmail.com>
2023-05-31 22:20:53 +0200

    Hello world

--- src/components/imageCard/index.jsx
+++ src/components/imageCard/index.jsx
@@ -1,20 +1,25 @@
-import Image from '../image'
-import Heading from '../heading';
-import Text from '../text';
-import './index.css'
+import Image from "../image";
+import Heading from "../heading";
+import Text from "../text";
+import "./index.css";
+import Button from "../button";
 
-
-const ImageCard = ({image, heading, text, link}) => {
+const ImageCard = ({ image, heading, text, link }) => {
   return (
     <div className="imageCard">
-      <Image src={image} alt={heading} className="imageCard__image"/>
-      <div className='imageCard__content'>
-        <Heading level={3}>{heading}</Heading>
-        <Text className="imageCard__text"> {text} </Text>
-        <a href={link} className='imageCard__link'>Read More</a>
+      <Image src={image} alt={heading} className="imageCard__image" />
+      <div className="imageCard__content">
+        <div>
+          <Heading level={3}>{heading}</Heading>
+          <Text size='12px' className="imageCard__text"> {text} </Text>
+        </div>
+        {/* <a href={link} className='imageCard__link'>Read More</a> */}
+        <Button use="text" href={link} color="secondary" size="small">
+          Read More
+        </Button>
       </div>
     </div>
-  )
+  );
 };
 
 export default ImageCard;
--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -1,12 +1,50 @@
-import './index.css';
+import { useState } from "react";
+import "./index.css";
+
+import contactFormModel from "../../models/ContactForm.model";
+import Heading from "../../components/heading";
+import Text from "../../components/text";
+import Form from "../../components/form";
+import Button from "../../components/button";
+
 
 const Contact = () => {
+  const [formState, setFormState] = useState({});
+
+  const InputChangeHandler = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    setFormState((prevState) => ({ ...prevState, [field]: value }));
+    console.log(formState)
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("Submitting... ", formState)
+    //validation
+    //sending data
+  }
 
   return (
     <div className="contact">
-      :)
+      <div className="container">
+        <div className="content">
+          <Heading level={2}>Contact</Heading>
+          <Text>
+            Questions or concerns? Just fill out the form below and our support
+            team will get back to you within 24 hours
+          </Text>
+        </div>
+
+        <div className="contact-form-wrapper">
+          <Form inputRows={contactFormModel} inputHandler={InputChangeHandler} formState={formState} />
+          <Button use="button" href="#" color="primary" type="submit" onClick={submitHandler}>
+            Submit Now
+          </Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Contact;
